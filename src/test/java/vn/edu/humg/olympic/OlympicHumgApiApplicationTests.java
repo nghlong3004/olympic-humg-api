3d77bd61114990d09d9429fb2d467c7413d55faf
nghlong3004 <nghlong3004@gmail.com>
2025-11-19 00:53:39 +0700

    Initialize Spring Boot project for Olympic API

---
+++ src/test/java/vn/edu/humg/olympic/OlympicHumgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.humg.olympic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicHumgApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
